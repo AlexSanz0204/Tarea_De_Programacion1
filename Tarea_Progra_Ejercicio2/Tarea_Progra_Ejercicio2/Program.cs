079c8577436f327918e763b6591eeeefd5207ed9
AlexSanz02 <zs0128032021@unab.edu.sv>
2022-02-22 13:55:31 -0600

    Tarea de Programación I

---
+++ Tarea_Progra_Ejercicio2/Tarea_Progra_Ejercicio2/Program.cs
@@ -0,0 +1,53 @@
+﻿//Hecho por: Alexander Enrique Zelaya Sánchez
+
+using System;
+using System.Collections;
+public class Ejemplo2
+{
+
+    public static void Main()
+    {
+        Queue miQ = new Queue();
+        miQ.Enqueue(1);
+        miQ.Enqueue(2);
+        miQ.Enqueue(3);
+        miQ.Enqueue(4);
+        miQ.Enqueue(5);
+
+        Console.WriteLine("\nElementos guardados: ");
+        Console.WriteLine("\n{0}", miQ.Count);
+        Console.Write("\nValores de la cola: ");
+        PrintValues(miQ);
+
+        Console.WriteLine("Removiendo un elemento: \n{0}", miQ.Dequeue());
+
+        Console.Write("\nMostrando valores: ");
+        PrintValues(miQ);
+
+
+        Console.WriteLine("Removiendo otro elemento: \n{0}", miQ.Dequeue());
+
+
+        Console.Write("\nMostrando valores actualizados: ");
+        PrintValues(miQ);
+
+
+        Console.WriteLine("\nMostrando primer elemento sin haber sido removidos\n{0}", miQ.Peek());
+
+        Console.Write("\nMostrando los valores actualizados: ");
+        PrintValues(miQ);
+
+        miQ.Clear();
+
+        Console.WriteLine("\nlos valores han sido Limpiados");
+        Console.WriteLine("\n{0}", miQ.Count);
+        Console.Write("\nValores en la cola:");
+        PrintValues(miQ);
+    }
+    public static void PrintValues(IEnumerable myCollection)
+    {
+        foreach (Object obj in myCollection)
+            Console.Write("\n {0}", obj);
+        Console.WriteLine();
+    }
+}
---
+++ tarea_programación/tarea_programación/Program.cs
@@ -0,0 +1,53 @@
+﻿//Hecho por: Alexander Enrique Zelaya Sánchez
+
+using System;
+using System.Collections.Generic;
+
+class Ejemplo1
+{
+    public static void Main()
+    {
+        Stack<string> nombres = new Stack<string>();
+        nombres.Push("Alexander");
+        nombres.Push("Enrique");
+        nombres.Push("Karen");
+        nombres.Push("Gabriela");
+        nombres.Push("Wendy");
+        nombres.Push("José");
+
+        foreach (string nombre in nombres)
+        {
+            Console.WriteLine(nombre);
+        }
+
+        Console.WriteLine("\nEliminando '{0}'", nombres.Pop());
+        Console.WriteLine("\nItem a desapilar: {0}", nombres.Peek());
+        Console.WriteLine("\nEliminando '{0}'", nombres.Pop());
+
+        Stack<string> stack2 = new Stack<string>(nombres.ToArray());
+
+        Console.WriteLine("\nPrimer copia: ");
+        foreach (string nombre in stack2)
+        {
+            Console.WriteLine(nombre);
+        }
+
+        string[] array2 = new string[nombres.Count * 2];
+        nombres.CopyTo(array2, nombres.Count);
+
+
+        Stack<string> stack3 = new Stack<string>(array2);
+
+        Console.WriteLine("\nSegunda copia,incluyendo duplicados y nulos: ");
+        foreach (string nombre in stack3)
+        {
+            Console.WriteLine(nombre);
+        }
+        Console.WriteLine("\nVerificando si contiene la pila numero 2  contiene el nombre Manuel");
+        Console.WriteLine("\nLa pila numero 2 contiene (\"Manuel\") = {0}",
+            stack2.Contains("Manuel"));
+        Console.WriteLine("\nLimpiando la pila numero 2");
+        stack2.Clear();
+        Console.WriteLine("\n{0}", stack2.Count);
+    }
+}
